7b6e81ef4c1bec61dc195c1467ac1b88ea5f6e19
amidrakhman <amidrakhman@gmail.com>
2025-09-22 21:11:50 +0700

    finalisasi

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const ai = new GoogleGenAI({});
 // inisialisasi model AI
 const geminiModels = {
   text: "gemini-2.5-flash-lite",
+  chat: "gemini-2.5-pro",
   image: "gemini-2.5-flash",
   audio: "gemini-2.5-flash-lite",
   document: "gemini-2.5-flash-lite"
@@ -41,6 +42,74 @@ app.post('/generate-text', async (req, res) => {
   res.status(200).json({
     reply: response.text
   });
+
+});
+
+app.post('/chat', async (req, res) => {
+  const { conversation } = req.body;
+
+  // cek validasi array atau bukan
+  if (!conversation || !Array.isArray(conversation)) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: 'Percakapan tidak valid!'
+    });
+  }
+
+  // cek integritas data
+  let dataIsInvalid =  false; //semantic
+  
+  conversation.forEach(item => {
+    if (!item) {
+      dataIsInvalid = true;
+    } else if (typeof item !== 'object') {
+      dataIsInvalid = true;
+    } else if (!item.role || !item.message) {
+      dataIsInvalid = true;
+    }
+  
+  });
+
+  if(dataIsInvalid) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: 'Ada Data tidak valid pada percakapan yang dikirim'
+    });
+}
+
+// mapping
+const contents = conversation.map(item => {
+    return {
+      role: item.role,
+      parts: [
+        { text: item.message}
+      ]   
+    } 
+});
+
+try {
+  const aiResponse = await ai.models.generateContent({
+    model: geminiModels.chat,
+    contents
+  });
+  
+  return res.status(200).json({
+    success: true,
+    data: aiResponse.text,
+    message: null
+  });
+
+} catch (e) {
+  console.log({ e });
+  return res.status(500).json({
+    success: false,
+    data: null,
+    message: e.message
+  });
+}
+
 
 });
 
